8955b642649fe4561f59bca300b85ab734595c7e
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-01-19 22:54:32 +0000

    FIX

--- tests/testthat/test_get_legislators.R
+++ tests/testthat/test_get_legislators.R
@@ -1,8 +1,4 @@
-
 test_that("get_legislators", {
   expect_equal(nrow(get_legislators(term = 2)$data),
                nrow(jsonlite::fromJSON("https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=02=&fileType=json")$dataList))
 })
-
-
-
