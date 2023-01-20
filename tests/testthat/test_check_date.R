4de5be9a1aba246dee0270c530056c3ad0ec78d7
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-01-20 01:42:22 +0000

    ADD
    test

--- tests/testthat/test_check_date.R
+++ tests/testthat/test_check_date.R
@@ -3,3 +3,7 @@ test_that("check_date", {
   expect_equal(check_date("1050521"), as.Date("2016-05-21"))
   expect_equal(check_date("1020521"), as.Date("2013-05-21"))
 })
+
+test_that("api_check", {
+  expect_equal(api_check(1031020, 1031025), api_check(1031020, 1031025))
+})
