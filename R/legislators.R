b4c460839576731f9629061328409737a41f7659
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-01-21 04:21:13 +0000

    UPDATE

--- R/legislators.R
+++ R/legislators.R
@@ -27,38 +27,45 @@
 #'get_legislators()
 #'@seealso
 #'\url{https://data.ly.gov.tw/getds.action?id=16}
-
-
+#'
 get_legislators <- function(term = NULL, verbose = TRUE) {
   legisTaiwan::check_internet()
   if (is.null(term)) {
+    # request full data
     set_api_url <- paste("https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=",
                          term, "=&fileType=json", sep = "")
-  }else {
-    attempt::stop_if_all(term, is.character, msg = "use numeric format only")
+  } else {
+    attempt::stop_if_all(term, is.character, msg = "use numeric format only.")
+    if (length(term) == 1) {
+      term <- sprintf("%02d", as.numeric(term))
+    } else if (length(term) > 1) {
+      term <- paste(sprintf("%02d", as.numeric(term)), collapse = "&")
+      message("The API is not allowed to query the data by multiple terms.")
+      message("The retrieved data might not be complete, \n")
+    }
     set_api_url <- paste("https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=",
-                         sprintf("%02d", as.numeric(term)), "=&fileType=json", sep = "")
+                         term, "=&fileType=json", sep = "")
   }
   tryCatch(
     {
       json_df <- jsonlite::fromJSON(set_api_url)
       df <- tibble::as_tibble(json_df$dataList)
+      attempt::stop_if_all(nrow(df) == 0, isTRUE, msg = "The query is unavailable")
       term <- paste(sort(as.numeric(unique(df$term))), collapse=" ", sep = ",")
-      attempt::stop_if_all(nrow(df) == 0, isTRUE, msg = "The query unavailable during the period of the dates in the API")
       if (isTRUE(verbose)) {
         cat(" Retrieved URL: \n", set_api_url, "\n")
         cat(" Retrieved term: ", term, "\n")
       }
       list_data <- list("title" = "the legislator's demographic information",
                         "query_time" = Sys.time(),
-                        "retrieved_term" = term,
+                        "retrieved_term" = unique(df$term),
                         "url" = set_api_url,
+                        "variable_names" = colnames(df),
+                        "manual_info" = "https://data.ly.gov.tw/getds.action?id=16",
                         "data" = df)
       return(list_data)
     },
     error = function(error_message) {
-      message("Warning: The data retrieved are not available in the database")
-      message("INFO: The error message from the Taiwan Legislative Yuan API or R:")
       message(error_message)
     }
   )
