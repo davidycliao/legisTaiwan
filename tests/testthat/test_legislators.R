f96af26b174838f31ceef99bbb7559d573448332
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-01-30 11:42:59 +0000

    Update test_legislators.R

--- tests/testthat/test_legislators.R
+++ tests/testthat/test_legislators.R
@@ -1,12 +1,6 @@
 test_that("get_legislators", {
-  expect_equal(nrow(get_legislators(term = 2)$data),
-               nrow(jsonlite::fromJSON("https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=02=&fileType=json")$dataList))
-  expect_equal(get_legislators(term = 2)$url,
-               "https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=02=&fileType=json")
-  expect_equal(get_legislators()$url,
-               "https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term==&fileType=json")
-  expect_equal(get_legislators(term = c(8,9))$queried_term,
-               "8")
+  expect_equal(nrow(get_legislators(term = 2)$data), 165)
+  expect_equal(get_legislators(term = 2)$url, "https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=02=&fileType=json")
+  expect_equal(get_legislators(term = 2, verbose = FALSE)$url, "https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=02=&fileType=json")
+  expect_equal(get_legislators(term = 8)$queried_term, "8")
 })
-
-
