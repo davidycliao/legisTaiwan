157447314ca82fd1e1c9390bd189ed0166803893
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-01-30 12:17:16 +0000

    Update test_legislators.R

--- tests/testthat/test_legislators.R
+++ tests/testthat/test_legislators.R
@@ -1,6 +1,6 @@
 test_that("get_legislators", {
   expect_equal(nrow(get_legislators(term = 2)$data), 165)
-  expect_equal(get_legislators(term = 2)$url, "https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=02=&fileType=json")
-  expect_equal(get_legislators(term = 2, verbose = FALSE)$url, "https://data.ly.gov.tw/odw/ID16Action.action?name=&sex=&party=&partyGroup=&areaName=&term=02=&fileType=json")
+  expect_equal(get_legislators(term = 2)$queried_term, "2")
+  expect_equal(get_legislators(term = 2, verbose = FALSE)$queried_term, "2")
   expect_equal(get_legislators(term = 8)$queried_term, "8")
 })
