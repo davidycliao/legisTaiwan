a5c132b4f529c7ec6638f395ff854c176751ba86
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-01-30 11:40:33 +0000

    Update test_bills.R

--- tests/testthat/test_bills.R
+++ tests/testthat/test_bills.R
@@ -1,8 +1,6 @@
 test_that("get_bill", {
-  expect_equal(get_bills(start_date = 1060120, end_date = 1070310,  proposer = "孔文吉")$retrieved_number,
-               length(jsonlite::fromJSON("https://www.ly.gov.tw/WebAPI/LegislativeBill.aspx?from=1060120&to=1070310&proposer=孔文吉&mode=json")$date))
-  expect_equal(get_bills(start_date = 1060120, end_date = 1070310,  proposer = "孔文吉", verbose = TRUE)$retrieved_number,
-               length(jsonlite::fromJSON("https://www.ly.gov.tw/WebAPI/LegislativeBill.aspx?from=1060120&to=1070310&proposer=孔文吉&mode=json")$date))
+  expect_equal(get_bills(start_date = 1060120, end_date = 1070310,  proposer = "孔文吉")$retrieved_number, 9))
+  expect_equal(get_bills(start_date = 1060120, end_date = 1070310,  proposer = "孔文吉", verbose = TRUE)$retrieved_number, 9))
   expect_equal(get_bills_2(term = 8, session_period = 1)$retrieved_number, 1155)
   expect_equal(get_bills_2(term = 8, session_period = 1, verbose = FALSE)$retrieved_number, 1155)
 })
