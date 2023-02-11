04d4b16d6c1bc309b0cd286a5d7f0af9555e0b21
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-02-11 03:30:13 +0100

    UPDATE

--- R/info.R
+++ R/info.R
@@ -118,7 +118,7 @@ get_variable_info <- function(param_) {
 #'Regarding Minguo calendar, please see \url{https://en.wikipedia.org/wiki/Republic_of_China_calendar}.
 
 review_session_info <- function(term){
-  attempt::stop_if_all(website_availability2(), isFALSE, msg = "the error from the API.")
+  attempt::stop_if_all(legisTaiwan::website_availability2(), isFALSE, msg = "the error from the API.")
   attempt::stop_if_all(term, is.null, msg = "use correct `term`")
   attempt::stop_if_all(term %in% 1:11, isFALSE, msg = "use correct `term`")
   url <- paste("https://npl.ly.gov.tw/do/www/appDate?status=0&expire=",
@@ -132,7 +132,7 @@ review_session_info <- function(term){
                      function(.){gsub("[[:space:]]", "", .)})
   df <- do.call(rbind, s)
   colnames(df) <- title
-  df <- as_tibble(df)
+  df <- tibble::as_tibble(df)
   return(df)
   }
 
