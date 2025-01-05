427d08e69fa19a2421be1550c0f609def5687ec1
davidycliao <davidycliao@gmail.com>
2025-01-05 01:18:26 +0000

    docs: update roxygen documentation and pkgdown site files

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,4 @@
-
-#' Register S3 methods and set up package
-#' @keywords internal
-.onLoad <- function(libname, pkgname) {
-  # 檢查並註冊 S3 方法
-  if (requireNamespace("vctrs", quietly = TRUE)) {
-    vctrs::s3_register("pillar::type_sum", "accel")
-  }
-}
-
-
-#' On package attach, display a startup message
-#'
+#' Package startup message
 #' @keywords internal
 .onAttach <- function(...) {
   # ANSI color codes
@@ -36,23 +24,21 @@
   )
 
   # Creating the messages
-  base_message1 <- paste0(dark_green, "legis", reset, rainbow_taiwan, " v0.2.1")
+  version <- utils::packageVersion("legisTaiwan")
+  base_message1 <- paste0(dark_green, "legis", reset, rainbow_taiwan, " v", version)
   message2 <- "## An R package connecting to the Taiwan Legislative API. ##"
 
   # Calculate padding to align message1 with message2
   message2_width <- nchar(message2)
-  base_message1_width <- nchar(base_message1) - (6 * 9)  # Subtract length of color codes (9 codes * 6 characters)
-  padding_needed <- message2_width - base_message1_width - 1  # -4 for the "## " and " ##"
+  base_message1_width <- nchar(base_message1) - (6 * 9)  # Subtract length of color codes
+  padding_needed <- message2_width - base_message1_width - 1
 
   # Create aligned message1
   message1 <- paste0("## ", base_message1, paste(rep(" ", padding_needed), collapse = ""), " ##")
 
-  # Display messages
-  packageStartupMessage(message1)
-  packageStartupMessage(message2)
-}
-
-#' @export
-type_sum.accel <- function(x) {
-  "accel"
+  # Display messages if not disabled
+  if (!isFALSE(getOption("legisTaiwan.startup.message", TRUE))) {
+    packageStartupMessage(message1)
+    packageStartupMessage(message2)
+  }
 }
